c01de2806b9ea687a85ad4b5538c1d30621d380a
imaluffy <msandesh258@gmail.com>
2025-02-16 01:27:05 +0530

    init commit

---
+++ src/main/java/com/trs/trs_user_service/TrsUserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.trs.trs_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrsUserServiceApplication.class, args);
+		System.out.println("hellow user");
+	}
+
+}
---
+++ src/test/java/com/trs/trs_user_service/TrsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trs.trs_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
